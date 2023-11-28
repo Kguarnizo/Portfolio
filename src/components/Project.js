eb0449de14f18dbd3480b65575f96825e1222893
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-11-28 07:24:08 -0500

    add logic for specific techstack icons to only render based on its pertaining project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import './Project.css';
+import { FaPython, FaReact, FaBootstrap, FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { BiLogoDjango, BiLogoPostgresq, BiLogoPostgresql, BiLogoTypescript} from "react-icons/bi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiSqlalchemy } from "react-icons/si";
 
-const Project = ({ title, techStack, description, image, demoLink, githubLink }) => {
+const iconsMap = {
+    Python: <FaPython icon={FaPython} />,
+    Django: <BiLogoDjango icon={BiLogoDjango} />,
+    Postgresql: <BiLogoPostgresql icon={BiLogoPostgresql} />,
+    TypeScript: <BiLogoTypescript icon={BiLogoTypescript} />,
+    React: <FaReact icon={FaReact} />,
+    Bootstrap: <FaBootstrap icon={FaBootstrap} />,
+    HTML: <FaHtml5 icon={FaHtml5} />,
+    CSS: <FaCss3Alt icon={FaCss3Alt} />,
+    Javascript: <IoLogoJavascript icon={IoLogoJavascript} />,
+    SQLAlchemy: <SiSqlalchemy icon={SiSqlalchemy} />
+};
+
+
+const Project = ({ title, description, image, demoLink, githubLink, techStack }) => {
+    const getIconComponent = (icon) => {
+        return iconsMap[icon];
+    };
+
+    const getIconsJSX = () => {
+        return techStack.map((icon, index) => {
+            return (
+                <div key={index}>
+                    {getIconComponent(icon)}
+                </div>
+            );
+        });
+    };
+    
+    
     return (
         <li className="project-item">
             <img src={image} alt="projects"/>
             <h3>{title}</h3>
             <p>{description}</p>
-            <div>
+            <div className="project-icons">
+                {getIconsJSX()}
+            </div>
+            <div className="project-links">
                 <a 
+                    id="demo"
                     target="_blank" 
                     rel="noopener noreferrer"
                     href={demoLink}
@@ -15,12 +53,16 @@ const Project = ({ title, techStack, description, image, demoLink, githubLink })
                 Demo
                 </a>
                 <a
+                    id="github"
                     target="_blank" 
                     rel="noopener noreferrer"
                     href={githubLink}
                 >
                 Github
                 </a>
+            </div>
+            <div>
+
             </div>
         </li>
     );
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import Project from '../Project';
+import './Projects.css';
 
 const Projects = ({projectList}) => {
 
     console.log("this is the list of projects", projectList)
+
     const getProjectListJSX = (projectList) => {
         return projectList.map((project, index) => {
             return (
                 <div key={index}>
                     <Project 
+                        // projectID={index}
                         title={project.title} 
                         description={project.description}
                         image={project.image}
                         demoLink={project.demoLink}
                         githubLink={project.githubLink}
+                        techStack={project.techStack}
                     />
                 </div>
             );
         });
     };
     return (
-        <div>
-            <h2>My Projects</h2>
-            <div className="project-list-container">
+        <div className="projects-container">
+            <h2 id="projects">My Projects</h2>
+            <div className="projects-list-container">
                 <ul>{getProjectListJSX(projectList)}</ul>
             </div>
         </div>
