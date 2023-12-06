5f680664326485184551e0e520f65b15d806a429
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-12-05 19:17:38 -0500

    update navbar to blur out after scrolling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,6 +4,7 @@ import './Navigation.css';
 
 const Navigation = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [colorNav, setColorNav] = useState(false)
   
   const toggleOpen = () => {
     setMenuOpen(!menuOpen);
@@ -13,31 +14,43 @@ const Navigation = () => {
     setMenuOpen(false);
   };
 
+  const changeColor = () => {
+    if (window.scrollY >= 50) {
+      setColorNav(true)
+    } else {
+      setColorNav(false)
+    }
+  }
+
+  window.addEventListener("scroll", changeColor)
+
+  const navbarClass = `navbar ${colorNav ? "scrolled" : ""}`;
+
   return (
-    <nav className="navbar">
-          <Link to="/" className="title">
-            KatherineGuarnizo.
-          </Link>
-          {/* <span id="logo-period">.</span> */}
-          <div className="menu" onClick={toggleOpen}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <ul className={menuOpen ? "open" : ""}>
-            <li>
-                <NavLink to="/about" onClick={closeMenu}>about</NavLink>
-            </li>
-            <li>
-                <NavLink to="/projects" onClick={closeMenu}>projects</NavLink>
-            </li>
-            <li>
-                <NavLink to="/skills" onClick={closeMenu}>skills</NavLink>
-            </li>
-            <li>
-                <NavLink to="/contact" onClick={closeMenu}>contact</NavLink>
-            </li>
-          </ul>
+    <nav className={navbarClass}>
+      <Link to="/" className="title">
+        KatherineGuarnizo
+        <span id="logo-period">.</span>
+      </Link>
+    <div className="menu" onClick={toggleOpen}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <ul className={menuOpen ? "open" : ""}>
+      <li>
+          <NavLink to="/about" onClick={closeMenu}>about</NavLink>
+      </li>
+      <li>
+          <NavLink to="/projects" onClick={closeMenu}>projects</NavLink>
+      </li>
+      <li>
+          <NavLink to="/skills" onClick={closeMenu}>skills</NavLink>
+      </li>
+      <li>
+          <NavLink to="/contact" onClick={closeMenu}>contact</NavLink>
+      </li>
+    </ul>
     </nav>
   );
 };
